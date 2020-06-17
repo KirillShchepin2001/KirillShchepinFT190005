f8163483595d517624f902cf6bf3cc785176d33f
KirillShchepin2001 <67072157+KirillShchepin2001@users.noreply.github.com>
2020-06-17 23:53:44 +0500

    Add files via upload

---
+++ Лабораторная 1 Кирилл Щепин/Лабораторная 1 Кирилл Щепин/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace KirSchep
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.x = new System.Windows.Forms.TextBox();
+            this.y = new System.Windows.Forms.TextBox();
+            this.z = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.alp = new System.Windows.Forms.TextBox();
+            this.bet = new System.Windows.Forms.TextBox();
+            this.gam = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.xy = new System.Windows.Forms.TextBox();
+            this.zx = new System.Windows.Forms.TextBox();
+            this.xz = new System.Windows.Forms.TextBox();
+            this.yx = new System.Windows.Forms.TextBox();
+            this.zy = new System.Windows.Forms.TextBox();
+            this.yz = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.t1 = new System.Windows.Forms.TextBox();
+            this.t3 = new System.Windows.Forms.TextBox();
+            this.t2 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(401, 465);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(430, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Масштабировать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // x
+            // 
+            this.x.Location = new System.Drawing.Point(430, 46);
+            this.x.Name = "x";
+            this.x.Size = new System.Drawing.Size(100, 20);
+            this.x.TabIndex = 2;
+            // 
+            // y
+            // 
+            this.y.Location = new System.Drawing.Point(430, 72);
+            this.y.Name = "y";
+            this.y.Size = new System.Drawing.Size(100, 20);
+            this.y.TabIndex = 3;
+            // 
+            // z
+            // 
+            this.z.Location = new System.Drawing.Point(430, 98);
+            this.z.Name = "z";
+            this.z.Size = new System.Drawing.Size(100, 20);
+            this.z.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(642, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Повернуть";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // alp
+            // 
+            this.alp.Location = new System.Drawing.Point(642, 46);
+            this.alp.Name = "alp";
+            this.alp.Size = new System.Drawing.Size(100, 20);
+            this.alp.TabIndex = 6;
+            // 
+            // bet
+            // 
+            this.bet.Location = new System.Drawing.Point(642, 72);
+            this.bet.Name = "bet";
+            this.bet.Size = new System.Drawing.Size(100, 20);
+            this.bet.TabIndex = 7;
+            // 
+            // gam
+            // 
+            this.gam.Location = new System.Drawing.Point(642, 98);
+            this.gam.Name = "gam";
+            this.gam.Size = new System.Drawing.Size(100, 20);
+            this.gam.TabIndex = 8;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(430, 124);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Сдвинуть";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // xy
+            // 
+            this.xy.Location = new System.Drawing.Point(430, 158);
+            this.xy.Name = "xy";
+            this.xy.Size = new System.Drawing.Size(100, 20);
+            this.xy.TabIndex = 10;
+            // 
+            // zx
+            // 
+            this.zx.Location = new System.Drawing.Point(430, 236);
+            this.zx.Name = "zx";
+            this.zx.Size = new System.Drawing.Size(100, 20);
+            this.zx.TabIndex = 11;
+            // 
+            // xz
+            // 
+            this.xz.Location = new System.Drawing.Point(430, 210);
+            this.xz.Name = "xz";
+            this.xz.Size = new System.Drawing.Size(100, 20);
+            this.xz.TabIndex = 12;
+            // 
+            // yx
+            // 
+            this.yx.Location = new System.Drawing.Point(430, 184);
+            this.yx.Name = "yx";
+            this.yx.Size = new System.Drawing.Size(100, 20);
+            this.yx.TabIndex = 13;
+            // 
+            // zy
+            // 
+            this.zy.Location = new System.Drawing.Point(430, 291);
+            this.zy.Name = "zy";
+            this.zy.Size = new System.Drawing.Size(100, 20);
+            this.zy.TabIndex = 14;
+            // 
+            // yz
+            // 
+            this.yz.Location = new System.Drawing.Point(430, 265);
+            this.yz.Name = "yz";
+            this.yz.Size = new System.Drawing.Size(100, 20);
+            this.yz.TabIndex = 15;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(642, 124);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "Перенести";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // t1
+            // 
+            this.t1.Location = new System.Drawing.Point(642, 158);
+            this.t1.Name = "t1";
+            this.t1.Size = new System.Drawing.Size(100, 20);
+            this.t1.TabIndex = 17;
+            // 
+            // t3
+            // 
+            this.t3.Location = new System.Drawing.Point(642, 210);
+            this.t3.Name = "t3";
+            this.t3.Size = new System.Drawing.Size(100, 20);
+            this.t3.TabIndex = 18;
+            // 
+            // t2
+            // 
+            this.t2.Location = new System.Drawing.Point(642, 184);
+            this.t2.Name = "t2";
+            this.t2.Size = new System.Drawing.Size(100, 20);
+            this.t2.TabIndex = 19;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(430, 430);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 47);
+            this.button6.TabIndex = 20;
+            this.button6.Text = "Поместить в центр";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(754, 489);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.t2);
+            this.Controls.Add(this.t3);
+            this.Controls.Add(this.t1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.yz);
+            this.Controls.Add(this.zy);
+            this.Controls.Add(this.yx);
+            this.Controls.Add(this.xz);
+            this.Controls.Add(this.zx);
+            this.Controls.Add(this.xy);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.gam);
+            this.Controls.Add(this.bet);
+            this.Controls.Add(this.alp);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.z);
+            this.Controls.Add(this.y);
+            this.Controls.Add(this.x);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "KirSchep";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picture;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox x;
+        private System.Windows.Forms.TextBox y;
+        private System.Windows.Forms.TextBox z;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox alp;
+        private System.Windows.Forms.TextBox bet;
+        private System.Windows.Forms.TextBox gam;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox xy;
+        private System.Windows.Forms.TextBox zx;
+        private System.Windows.Forms.TextBox xz;
+        private System.Windows.Forms.TextBox yx;
+        private System.Windows.Forms.TextBox zy;
+        private System.Windows.Forms.TextBox yz;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox t1;
+        private System.Windows.Forms.TextBox t3;
+        private System.Windows.Forms.TextBox t2;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Лабораторная 1 Кирилл Щепин/Лабораторная 1 Кирилл Щепин/Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KirSchep
+
+{
+    public partial class Form1 : Form
+
+    {
+        class Point
+        {
+            public double x;
+            public double y;
+            public double z;
+            public double H;
+
+            static public Point Parse(string str)
+            {
+                Point point = new Point();
+
+                string[] st = str.Split(' ');
+
+                point.x = double.Parse(st[0]);
+                point.y = double.Parse(st[1]);
+                point.z = double.Parse(st[2]);
+                point.H = double.Parse(st[3]);
+
+                return point;
+            }
+        }
+
+        class Line
+        {
+            public int begin;
+            public int end;
+
+            static public Line Parse(string str)
+            {
+                Line line = new Line();
+
+                string[] a = str.Split(' ');
+
+                line.begin = int.Parse(a[0]);
+                line.end = int.Parse(a[1]);
+
+                return line;
+            }
+        }
+
+        List<Point> toch = new List<Point>();
+        List<Line> line = new List<Line>();
+     
+            Graphics g;
+
+        void Schitivanie()
+        {
+            StreamReader a = new StreamReader("C:\\Kirill2001\\kirill.txt.txt");
+
+            var str = a.ReadLine();
+            while (true)
+            {
+
+                if (a.EndOfStream) break;
+                if (str == "*") break;
+                toch.Add(Point.Parse(str));
+                str = a.ReadLine();
+            }
+            str = a.ReadLine();
+            while (true)
+            {
+
+                if (a.EndOfStream) break;
+                if (str == "*") break;
+                line.Add(Line.Parse(str));
+                str = a.ReadLine();
+            }
+
+        }
+
+        void Vmeshenie()
+        {
+            double max1 = toch[0].x;
+            double min1 = toch[0].x;
+            double max2 = toch[0].y;
+            double min2 = toch[0].y;
+
+            for (int i = 0; i < toch.Count; i++)
+            {
+                if (toch[i].x > max1) max1 = toch[i].x;
+                if (toch[i].y > max2) max2 = toch[i].y;
+
+                if (toch[i].x < min1) min1 = toch[i].x;
+                if (toch[i].y < min2) min2 = toch[i].y;
+            }
+
+            double q;
+
+            if (pictureBox1.Height / (max2 - min2) > pictureBox1.Width / (max1 - min1))
+                q = pictureBox1.Width / (max1 - min1);
+            else q = pictureBox1.Height / (max2 - min2);
+
+
+            for (int i = 0; i < toch.Count; i++)
+            {
+                toch[i].x -= min1;
+                toch[i].y -= min2;
+
+                toch[i].x *= q;
+                toch[i].y *= q;
+                toch[i].z *= q;
+            }
+        }
+
+        void Risunok()
+        {
+            g.Clear(Color.White);
+
+            {
+                for (int i = 0; i < line.Count; i++)
+                {
+                    g.DrawLine(new Pen(Color.Red, 2),
+                        (float)toch[line[i].begin - 1].x, (float)toch[line[i].begin - 1].y,
+                        (float)toch[line[i].end - 1].x, (float)toch[line[i].end - 1].y);
+                }
+            }
+            pictureBox1.Invalidate();
+        }
+
+        void Sdvig(double x, double y, double z)
+        {
+
+            for (int i = 0; i < toch.Count; i++)
+            {
+                toch[i].x = toch[i].x + toch[i].H * x;
+                toch[i].y = toch[i].y + toch[i].H * y;
+                toch[i].z = toch[i].z + toch[i].H * z;
+            }
+        }
+
+        void Masshtab(double X, double Y, double Z)
+        {
+
+            for (int i = 0; i < toch.Count; i++)
+            {
+                toch[i].x = toch[i].x * X;
+                toch[i].y = toch[i].y * Y;
+                toch[i].z = toch[i].z * Z;
+            }
+        }
+
+        void Povorot(double alp, double bet, double gam)
+        {
+
+            double newX, newY, newZ;
+            for (int i = 0; i < toch.Count; i++)
+            {
+                newY = toch[i].y * Math.Cos(alp * 180 / Math.PI) - toch[i].z * Math.Sin(alp * 180 / Math.PI);
+                newZ = toch[i].y * Math.Sin(alp * 180 / Math.PI) + toch[i].z * Math.Cos(alp * 180 / Math.PI);
+                toch[i].y = newY;
+                toch[i].z = newZ;
+            }
+            for (int i = 0; i < toch.Count; i++)
+            {
+                newX = toch[i].x * Math.Cos(bet * 180 / Math.PI) + toch[i].z * Math.Sin(bet * 180 / Math.PI);
+                newZ = -toch[i].x * Math.Sin(bet * 180 / Math.PI) + toch[i].z * Math.Cos(bet * 180 / Math.PI);
+                toch[i].x = newX;
+                toch[i].z = newZ;
+            }
+            for (int i = 0; i < toch.Count; i++)
+            {
+                newY = toch[i].x * Math.Sin(gam * 180 / Math.PI) + toch[i].y * Math.Cos(gam * 180 / Math.PI);
+                newX = toch[i].x * Math.Cos(gam * 180 / Math.PI) - toch[i].y * Math.Sin(gam * 180 / Math.PI);
+                toch[i].y = newY;
+                toch[i].x = newX;
+            }
+        }
+
+        void Dvig(double xy, double xz, double yz, double zx, double yx, double zy)
+        {
+            for (int i = 0; i < toch.Count; i++)
+            {
+                toch[i].x = toch[i].x + toch[i].y * xy;
+            }
+            for (int i = 0; i < toch.Count; i++)
+            {
+                toch[i].x = toch[i].x + toch[i].z * xz;
+            }
+            for (int i = 0; i < toch.Count; i++)
+            {
+                toch[i].y = toch[i].y + toch[i].x * yx;
+            }
+            for (int i = 0; i < toch.Count; i++)
+            {
+                toch[i].y = toch[i].y + toch[i].z * yz;
+            }
+            for (int i = 0; i < toch.Count; i++)
+            {
+                toch[i].z = toch[i].z + toch[i].x * zx;
+            }
+            for (int i = 0; i < toch.Count; i++)
+            {
+                toch[i].x = toch[i].z + toch[i].y * zy;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            g = Graphics.FromImage(pictureBox1.Image);
+
+            Schitivanie();
+            Vmeshenie();
+            Risunok();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Masshtab(Double.Parse(x.Text),
+                Double.Parse(y.Text),
+                Double.Parse(z.Text));
+            Risunok();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Povorot(Double.Parse(alp.Text),
+                  Double.Parse(bet.Text),
+                  Double.Parse(gam.Text));
+            Risunok();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Dvig(Double.Parse(xy.Text),
+                              Double.Parse(yx.Text),
+                              Double.Parse(xz.Text),
+                              Double.Parse(zx.Text),
+                              Double.Parse(yz.Text),
+                              Double.Parse(zy.Text));
+           Risunok();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+           Sdvig(Double.Parse(t1.Text),
+                Double.Parse(t2.Text),
+                Double.Parse(t3.Text));
+            Risunok();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+           
+            Vmeshenie();
+            Risunok();
+            
+        }
+
+    }
+}
